14bca1aea7dc544fca013718fadd92afffd50a84
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-14 22:40:59 +0900

    変数

---
+++ variable.java
@@ -0,0 +1,11 @@
+class variable {
+ public static void main(String[]arges){
+ int number;
+ number = 3;
+ System.out.println(number);
+
+ String name;
+  name = "わんこ";
+  System.out.println(name);
+ }
+}
