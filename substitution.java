f162864c056b522f709dbd78404f97d96afb1e1f
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-15 20:24:49 +0900

    変数と代入

---
+++ substitution.java
@@ -0,0 +1,9 @@
+class substitution {
+  public  static void main(String[]ages){
+    int number = 8;
+    number *=7;
+    System.out.println(number);
+    number ++ ;
+    System.out.println(number);
+  }
+}
