add2c5cac9ade9e970edcdb49b2269e7e357df72
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-15 22:02:02 +0900

    変数　少数

---
+++ minority.java
@@ -0,0 +1,9 @@
+class minority {
+  public static void main(String[]ages){
+    double number1 = 8.5;
+    double number2 = 3.7;
+    System.out.println(number1+number2);
+    System.out.println(number1*number2);
+
+  }  
+}
