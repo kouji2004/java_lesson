94234402a726f296c307ea3b7f744018b05dd4d0
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-15 21:13:32 +0900

    変数の役割

---
+++ NumberVariables.java
@@ -0,0 +1,12 @@
+class NumberVariables {
+  public static void main(String[] ages) {
+    int length = 6;
+    int height = 8;
+
+    int rectangleArea = length * height;
+    System.out.println(rectangleArea);
+
+    int aiueo = length * height/2;
+    System.out.println(aiueo);
+  }
+}
