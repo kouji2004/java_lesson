fa8029d2ce0fe5f6fa591e01a822f92640d899b5
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-15 16:40:37 +0900

    変数の初期化

---
+++ vvariable.java
@@ -0,0 +1,10 @@
+class vvariable {
+  public static void main(String[]args){
+    int number = 4;
+    int number2 = 7;
+    System.out.println(number*number2);
+
+    String text = "こんにちは";
+    System.out.println("挨拶"+text);
+  }
+}
