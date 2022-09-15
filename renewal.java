eb2cf5f4cb9edbce0b618b7b10a2d7d8623cd1bb
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-15 17:49:23 +0900

    変数の更新

---
+++ renewal.java
@@ -0,0 +1,14 @@
+class renewal {
+  public static void main(String[]aeges){
+    int number = 34;
+    String text = "Ruby";
+    System.out.println(number);
+    System.out.println(text);
+
+    number = 46;
+    System.out.println(number);
+
+    text = "Java";
+    System.out.println(text);
+  }
+}
