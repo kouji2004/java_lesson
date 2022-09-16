a4f6f85742d9a29f81ca4eac053b5e3a2cec9584
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-16 21:00:48 +0900

    演習問題

---
+++ practice.java
@@ -0,0 +1,11 @@
+class practice {
+  public static void main(String[]ages){
+    String name = "miura";
+    int age = 12;
+    double height = 13.2;
+    double wight = 23.2;
+    double bmi = wight/(height*2);
+    System.out.println(bmi);
+    System.out.println(name);
+  }  
+}
